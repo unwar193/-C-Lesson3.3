9cfb9739a3ac8b2fad3547b32a7d8657c0460129
unwar193 <varan193@mail.ru>
2022-09-26 12:11:12 +0300

    added solution task 23

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine("Введите число больше 1: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 1){
+    Console.WriteLine("Вы ввели некорректное число, попробуйте ещё раз!"); 
+} else {
+    int i = 1;
+    while (i <= num){
+        Console.WriteLine(i*i*i);
+        i++;
+    }
+}
